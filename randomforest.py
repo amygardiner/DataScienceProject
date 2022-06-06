92d8afcfef209283d4be2bc1114525d10fbc882c
amygardiner <amy_rebecca_g@hotmail.co.uk>
2022-06-06 21:17:52 +0100

    AUC is wrong

--- randomforest.py
+++ randomforest.py
@@ -16,6 +16,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer 
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import roc_auc_score #inkeeping with literature
@@ -25,47 +26,42 @@ filepath="/Users/amygardiner/Documents/University/PGD/Proj/Data/Paid_labelled/20
 df = pd.read_csv(filepath, sep="\t")
 df['tweet_id'] = df['tweet_id'].str.replace("'"," ")
 
-tweets=[]
-labels=df['label']
+#mapping class strings to integers
+numericalmap = {'injured_or_dead_people':1, 'missing_trapped_or_found_people':2, 'displaced_people_and_evacuations':3, 'infrastructure_and_utilities_damage':4, 'donation_needs_or_offers_or_volunteering_services':5, 'caution_and_advice':6, 'sympathy_and_emotional_support':7, 'other_useful_information':8, 'not_related_or_irrelevant':9}
+df=df.applymap(lambda s: numericalmap.get(s) if s in numericalmap else s)
 
-for i in df['tweet_text']:
-    sent_processed=re.sub("@[A-Za-z0-9_]+","",i) #regular expression to remove mentions
-    sent_processed=re.sub(r'http\S+', '', sent_processed) #regex to remove URLs
-    sent_processed=re.sub('RT', '', sent_processed) #removing RT
-    sent_processed=re.sub("[^\w\s]", '', sent_processed) #removing punctuation
-    stop_words = set(stopwords.words('english'))
-    word_tokens = word_tokenize(sent_processed)
-    
-    filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
-    filtered_sentence = []
-  
-    for w in word_tokens:
-        if w not in stop_words:
-            filtered_sentence.append(w)
-    tweets.append(filtered_sentence)      
+train_tweets, test_tweets, train_labels, test_labels = train_test_split(df['tweet_text'], df['label'], test_size = 0.20, random_state = 0)
 
-train_tweets, test_tweets, train_labels, test_labels = train_test_split(tweets, labels, test_size = 0.20, random_state = 0)
+def preprocessor(text):
+    tweets=[]
+    for i in text:
+        sent_processed=re.sub("@[A-Za-z0-9_]+","",i) #regular expression to remove mentions
+        sent_processed=re.sub(r'http\S+', '', sent_processed) #regex to remove URLs
+        sent_processed=re.sub('RT', '', sent_processed) #removing RT
+        sent_processed=re.sub("[^\w\s]", '', sent_processed) #removing punctuation
+        stop_words = set(stopwords.words('english'))
+        word_tokens = word_tokenize(sent_processed)
+        
+        filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
+        filtered_sentence = []
+      
+        for w in word_tokens:
+            if w not in stop_words:
+                filtered_sentence.append(w)
+        tweets.append(filtered_sentence)
+    return tweets
 
-from itertools import chain
-train_tweets=list(chain.from_iterable(train_tweets))
+train_tweets=preprocessor(train_tweets)
+test_tweets=preprocessor(test_tweets)
 
-#vectorizer.fit(train_tweets)
-#X_train = vectorizer.transform(train_tweets)
-#X_test = vectorizer.transform(test_tweets)
+count_vec = MultiLabelBinarizer()
+mlb = count_vec.fit(train_tweets)
+X_train = mlb.transform(train_tweets)
+X_test = mlb.transform(test_tweets)
 
-vectorizer = CountVectorizer()
-X_train = vectorizer.fit_transform(train_tweets)
 
-"""
-
-x = df.drop(['label'], axis=1)
-y = df['label']
-
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.20, random_state = 0)
-
-regressor = RandomForestRegressor(n_estimators = 100, random_state = 0)
-regressor.fit(X_train, y_train)
+regressor = RandomForestRegressor(random_state = 0)
+regressor.fit(X_train, train_labels)
 y_pred = regressor.predict(X_test)
 
-print('ROC Score:', roc_auc_score(y, y_pred))
-"""
+print('ROC Score:', roc_auc_score(test_labels, y_pred,multi_class='ovr'))
